c6685130380ffa5e79271a30e9adbaeba4179892
Rafael L. Soares <rafael@gkult.net>
2024-01-04 15:28:16 -0300

    Basic app created

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const express = require
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.get('/', (request, response, next) => {
+  response.json({
+    message: "It's alive!",
+    name: process.env.FIRSTNAME,
+    port: process.env.PORT
+  })
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`${process.env.FIRSTNAME}, sua app está rodando na porta ${process.env.PORT}`) 
+})
